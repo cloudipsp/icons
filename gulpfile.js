44a6bd84336c484e68b78fd67abc98396767d054
Михайло Голубовський <lamerumiata@gmail.com>
2024-08-27 16:58:26 +0300

    refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,62 @@
-var gulp     = require('gulp');
-var del      = require('del');
-var svg2png = require('gulp-svg2png');
-var svgmin = require('gulp-svgmin');
-var fs = require('fs');
-var argv = require('minimist')(process.argv.slice(2));
+const gulp = require('gulp');
+const del = require('del');
+const svg2png = require('gulp-svg2png');
+const svgmin = require('gulp-svgmin');
+const fs = require('fs');
+const argv = require('minimist')(process.argv.slice(2));
 
-var convertToPng = function(path,size){
-    return gulp.src(path)
-        .pipe(svg2png({ width: size, height: size }))
-        .pipe(gulp.dest('dist/png/'.concat(size)));
+const svgPath = 'src/**/*.svg';
+
+const convertToPng = function (path, out, size) {
+  return gulp
+    .src(path)
+    .pipe(svg2png({ width: size, height: size }))
+    .pipe(gulp.dest(out + size));
 };
 
-var compressSvg = function(deps,out){
-  return gulp.src(deps)
-    .pipe(svgmin())
-    .pipe(gulp.dest(out));
+const compressSvg = function (path, out) {
+  return gulp.src(path).pipe(svgmin()).pipe(gulp.dest(out));
 };
 
 gulp.task('svg2png:32', function () {
-    return convertToPng('src/**/*.svg',32);
+  return convertToPng(svgPath, 'dist/png/', 32);
 });
 
 gulp.task('svg2png:64', function () {
-    return convertToPng('src/**/*.svg',64);
+  return convertToPng(svgPath, 'dist/png/', 64);
 });
 
 gulp.task('svg2png:128', function () {
-    return convertToPng('src/**/*.svg',128);
+  return convertToPng(svgPath, 'dist/png/', 128);
 });
 
-gulp.task('svg_copy', function () {
-    return compressSvg('src/**/*.svg', 'dist/svg')
+gulp.task('svg', function () {
+  return compressSvg(svgPath, 'dist/svg');
 });
 
-gulp.task('json_copy', function () {
-  return gulp.src('src/json/**/*.json', { encoding: false }).pipe(gulp.dest('dist/json'))
+gulp.task('json', function () {
+  return gulp
+    .src('src/json/**/*.json', { encoding: false })
+    .pipe(gulp.dest('dist/json'));
 });
 
-gulp.task('favicon_copy', function () {
-  return gulp.src('src/favicon/**/*', { encoding: false }).pipe(gulp.dest('dist/favicon'))
+gulp.task('favicon', function () {
+  return gulp
+    .src('src/favicon/**/*', { encoding: false })
+    .pipe(gulp.dest('dist/favicon'));
 });
 
-gulp.task('fonts_copy', function () {
-  return gulp.src('src/fonts/**/*', { encoding: false }).pipe(gulp.dest('dist/fonts'))
+gulp.task('fonts', function () {
+  return gulp
+    .src('src/fonts/**/*', { encoding: false })
+    .pipe(gulp.dest('dist/fonts'));
 });
 
 gulp.task('flags', function () {
-  return compressSvg('node_modules/flag-icon-css/flags/4x3/*.svg', 'dist/svg/flags')
+  return compressSvg(
+    'node_modules/flag-icon-css/flags/4x3/*.svg',
+    'dist/svg/flags',
+  );
 });
 
 gulp.task('version', function (done) {
@@ -56,24 +66,25 @@ gulp.task('version', function (done) {
   done();
 });
 
-gulp.task('clean', function(done) {
+gulp.task('clean', function (done) {
   del.sync(['dist/*']);
   done();
 });
 
-gulp.task('default',
+gulp.task(
+  'default',
   gulp.series(
     'clean',
     gulp.parallel(
       'version',
-      'svg_copy',
-      'json_copy',
-      'favicon_copy',
-      'fonts_copy',
+      'svg',
+      'json',
+      'favicon',
+      'fonts',
       'flags',
       'svg2png:32',
       'svg2png:64',
-      'svg2png:128'
-    )
-  )
+      'svg2png:128',
+    ),
+  ),
 );
